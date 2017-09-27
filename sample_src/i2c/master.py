b12aed50ef63f5e78a7e9bd25b014ff38675d09c
daichij <djameson@mines.edu>
2017-09-27 22:44:54 +0000

    Added RPi-end sending for I2C

--- sample_src/i2c/master.py
+++ sample_src/i2c/master.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
 
-print("Hello world")
+import smbus
+
+MOTOR_BUS = 1
+MOTOR_ADDR = 0x28
+
+bus = smbus.SMBus(MOTOR_BUS)
+
+msg = 'a'
+while (msg != '0'):
+    msg = input("What do?")
+    print(type(msg))
+    bus.write_byte(MOTOR_ADDR, int(msg))
 
