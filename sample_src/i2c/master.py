735314754a2bb75a4625f03c66d3a8b940904b38
daichij <djameson@mines.edu>
2017-10-05 01:01:36 +0000

    Started working on getting the i2c working bidirectionally

--- sample_src/i2c/master.py
+++ sample_src/i2c/master.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+# For documentation on smbus, look at the smbus c library
 import smbus
 
 MOTOR_BUS = 1
@@ -7,9 +8,15 @@ MOTOR_ADDR = 0x28
 
 bus = smbus.SMBus(MOTOR_BUS)
 
+bus.write_block_data(MOTOR_ADDR, 0, msg)
+
 msg = 'a'
 while (msg != '0'):
     msg = input("What do?")
-    print(type(msg))
-    bus.write_byte(MOTOR_ADDR, int(msg))
+
+    if msg == "write":
+        # Do a write
+        bus.write_byte(MOTOR_ADDR, int(msg))
+    elif msg == "read":
+        # Do a read
 
