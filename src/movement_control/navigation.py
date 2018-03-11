71017ea59acad120bd0be41c19956eb682ebf7c0
Kristin Farris <kfarris@mines.edu>
2018-03-11 15:45:51 -0600

    Added instructions text file and navigation file

---
+++ src/movement_control/navigation.py
@@ -0,0 +1,47 @@
+def readInstructionFile():
+    file = open("instructions.txt", "r")
+    instructList = []
+    for line in file:
+        instructList.append(line.split())
+    return instructList
+
+def sendTargetPosition(x,y,r,d)
+    #TODO actually send this stuff over
+
+def executeInstuctions(instructList):
+    for instuct in instructList:
+        name = instruct[0]
+        if name == "Dump":
+            color = instruct[1]
+            #TODO add dump instructions
+        else:
+            x,y,r = instruct[1], instruct[2], instruct[3]
+            if name == "DRB":
+                d = -1
+            else:
+                d = 1
+            sendTargetPosition(x,y,r,d)
+            #TODO wait for arduino
+            if len(instruct) > 4:
+                atGoal = False
+                check = instruct[4]
+                d = 0
+                while not atGoal:
+                    if check == "toCross":
+                        #TODO do the toCross checker
+                        x_c, y_c, r_c = featureDectector.checkCross()
+                    elif check == "toFork":
+                        refAngle = instruct[5]
+                        #TODO do the toFork checker
+                    elif check == "toLine":
+                        #TODO do the toLine checker
+                        refAngle = instruct[5]
+                    else
+                        #TODO do the toL checker
+                    
+                    #TODO adjust feature detection values to adjust for
+                    #     orientation
+                    
+                    sendCurrentPosition(x_c,y_c,r_c,d)#wait
+
+                    atGoal = isAtGoal((x,y,r), (x_c, y_c, r_c))
