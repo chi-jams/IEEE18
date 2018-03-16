e96dc347bae2b3d17979a7f44436c4f567b59616
daichij <djameson@mines.edu>
2018-03-16 00:39:35 -0600

    Got arduino-end of navpi-i2c interface functional

--- src/movement_control/arduino_modules/NavDuino/NavDuino.ino
+++ src/movement_control/arduino_modules/NavDuino/NavDuino.ino
@@ -7,55 +7,97 @@
 */
 
 #include <Wire.h>
-#include <stdio.h> // TODO: Remove when debug is done
+
+#include <stdio.h>
+#define MAX_BUF 32
 
 const int I2C_ADDR = 0x42;
 
-int x, y, r;
-int dir;
+const int GET_TARG_POS = 0;
+const int GET_CUR_POS = 1;
+
+enum POS_IDX {X, Y, R, NUM_AXIS};
+
+volatile int target[NUM_AXIS];
+volatile int dir;
+
+volatile int pos[NUM_AXIS];
+
+volatile int completed;
 
 // TODO: Remove debug code
-int l_x, l_y, l_r;
+int l_target[NUM_AXIS];
 int l_dir;
+
+int l_pos[NUM_AXIS];
 
 void setup() {
     Serial.begin(9600);
 
     Wire.begin(I2C_ADDR);
-    Wire.onReceive(parseAction);
+    Wire.onReceive(getPosition);
+    Wire.onRequest(checkDone);
 
+    completed = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-    if (l_x != x || l_y != y || l_r != r) {
-        Serial.println(sprintf("Pos: (%d, %d, %d)", x, y, r);
-        l_x = x;
-        l_y = y;
-        l_r = r;
+    char buf[MAX_BUF];
+    if (target[X] != l_target[X] || target[Y] != l_target[Y] 
+        || target[R] != l_target[R] || dir != l_dir) {
+        sprintf(buf, "Target: (%d, %d, %d) %d", target[X], target[Y], target[R], dir);
+        Serial.println(buf);
+        for (int i = 0; i < 3; i++)
+            l_target[i] = target[i];
+        l_dir = dir;
+    }
+    else if (pos[X] != l_pos[X] || pos[Y] != l_pos[Y] || pos[R] != l_pos[R] 
+             || dir != l_dir) {
+        sprintf(buf, "Pos: (%d, %d, %d)", pos[X], pos[Y], pos[R]);
+        Serial.println(buf);
+        for (int i = 0; i < 3; i++)
+            l_pos[i] = pos[i];
     }
 }
 
 void getPosition(int num_bytes) {
-    Serial.println(sprintf("There are %d bytes", num_bytes));
-
-    if (num_bytes != 6 || num_bytes != 8)
-        Serial.println("Wtf");
-
-    if (num_bytes == 10 || num_bytes == 9) {
-        Serial.println("Oh it's the command");
-        int cmd = 0;
-        int d = Wire.read();
-        if (num_buytes == 10) {
-            cmd = (d << 8) | Wire.read();
-        } 
-        Serial.println(sprintf("cmd: %d", cmd));
+    // The smbus implementation sends over a command and the number of bytes it has in its payload
+    // before it actually, dir sends over the real data
+    int cmd = Wire.read();
+    int bytes = Wire.read();
+    
+    // Do a sanity check to make sure that the data received follows the correct format
+    if (num_bytes == bytes + 2) {
+        if (cmd == GET_TARG_POS) {
+            for(int i = 0; i < 3; i++)
+                target[i] = i2cGetInt();
+            dir = i2cGetInt();
+        }
+        else if (cmd == GET_CUR_POS) {
+            for (int i = 0; i < 3; i++)
+                pos[i] = i2cGetInt();
+        }
+        else {
+            dumpData();
+        }
     }
+    else {
+        // We have an unexpected message, throw it out.
+        dumpData();   
+    }
+}
 
-    // lol wtf am I doing
-    x = (Wire.read() << 8) | Wire.read();
-    y = (Wire.read() << 8) | Wire.read();
-    r = (Wire.read() << 8) | Wire.read();
+void checkDone() {
+    Wire.write(completed);
+    completed = completed == 0 ? 1 : 0;
+}
+
+// Reads the next 2 bytes from the i2c bus and splices them together to make a signed 16-bit integer.
+int i2cGetInt() {
+    return ((Wire.read() << 8) | Wire.read());
+}
+
+void dumpData() {
+    while (Wire.available()) Wire.read();
 }
 
